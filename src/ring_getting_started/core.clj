74588d138fe0438071670644dd1573452a9ff38f
ofadeyi <ofadeyi@gmail.com>
2017-02-21 12:45:01 +0000

    LEARN-004 | Ola | Define handler(s) and custom middleware

--- src/ring_getting_started/core.clj
+++ src/ring_getting_started/core.clj
@@ -5,3 +5,19 @@
   {:status 200,
    :headers {"Content-Type" "text/html"},
    :body "Hello World"})
+
+;; Concepts
+; Handlers, Requests, Responses
+(defn what-is-my-ip [request]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body (:remote-addr request)})
+
+; Middleware
+(defn wrap-content-type [handle content-type]
+  (fn [request]
+    (let [response (handler request)]
+      (assoc-in response [:headers "Content-Type"] content-type))))
+; apply middleware to an handler
+(def app
+  (wrap-content-type what-is-my-ip "text/html"))
