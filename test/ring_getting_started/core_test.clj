f6d2c547a8dd1ee798cef2b2cb903708ee0f1ffd
ofadeyi <ofadeyi@gmail.com>
2017-02-21 11:59:28 +0000

    LEARN-001 | Ola | Initial commit

---
+++ test/ring_getting_started/core_test.clj
@@ -0,0 +1,7 @@
+(ns ring-getting-started.core-test
+  (:require [clojure.test :refer :all]
+            [ring-getting-started.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
