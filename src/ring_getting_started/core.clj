c9390480fc25d0b8a00ab6960907898da0442acc
ofadeyi <ofadeyi@gmail.com>
2017-02-21 12:27:32 +0000

    LEARN-003 | Ola | HelloWorld  handler

--- src/ring_getting_started/core.clj
+++ src/ring_getting_started/core.clj
@@ -1,6 +1,7 @@
 (ns ring-getting-started.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; Getting Started
+(defn handler [request]
+  {:status 200,
+   :headers {"Content-Type" "text/html"},
+   :body "Hello World"})
